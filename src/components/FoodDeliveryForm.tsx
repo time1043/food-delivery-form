56cc3ecce835483dc2c4110c5fc3c237f092f751
oswin902m <1665434994@qq.com>
2026-04-28 17:11:14 +0800

    Validation in RHF

--- src/components/FoodDeliveryForm.tsx
+++ src/components/FoodDeliveryForm.tsx
@@ -12,7 +12,15 @@ type FoodDeliveryFormType = {
 const RenderCountComponent = RenderCount();
 
 function FoodDeliveryForm() {
-  const { register, handleSubmit } = useForm<FoodDeliveryFormType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FoodDeliveryFormType>({
+    // mode: "onSubmit",
+    // reValidateMode: "onChange",
+    // shouldFocusError: true,
+    criteriaMode: "all",
     defaultValues: {
       orderNo: new Date().valueOf(),
       customerName: "",
@@ -33,6 +41,7 @@ function FoodDeliveryForm() {
     <form
       className="m-4"
       autoComplete="off"
+      noValidate
       onSubmit={handleSubmit(onSumbit, onError)}
     >
       <h1>RHF</h1>
@@ -50,6 +59,11 @@ function FoodDeliveryForm() {
               {...register("orderNo")}
             />
             <label>Order No</label>
+            {errors.orderNo && (
+              <div className="invalid-feedback text-start">
+                {errors.orderNo?.message}
+              </div>
+            )}
           </div>
         </div>
 
@@ -57,13 +71,20 @@ function FoodDeliveryForm() {
           <div className="form-floating">
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${errors.mobile ? "is-invalid" : ""}`}
               placeholder="Mobile ..."
               {...register("mobile", {
-                required: "Mobile number is required.",
+                required: "Mobile is required.",
+                minLength: { value: 10, message: "Must be 10 digits." },
+                maxLength: { value: 11, message: "Must be 11 digits." },
               })}
             />
             <label>Mobile</label>
+            {errors.mobile && (
+              <div className="invalid-feedback text-start">
+                {errors.mobile?.message}
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -73,13 +94,34 @@ function FoodDeliveryForm() {
           <div className="form-floating">
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${errors.customerName ? "is-invalid" : ""}`}
               placeholder="Customer Name ..."
               {...register("customerName", {
                 required: "Customer Name is required.",
+                minLength: { value: 3, message: "Must be at least 3 chars." },
+                pattern: {
+                  value: /[A-Z]/,
+                  message: "Must include at least one uppercase letter.",
+                },
+                // validate: (value) =>
+                //   value.trim() !== "" || "Customer Name cannot be empty spaces",
               })}
             />
             <label>Customer Name</label>
+            {errors.customerName?.types &&
+              // <>
+              //   <div className="invalid-feedback text-start">
+              //     {errors.customerName?.types.minLength}
+              //   </div>
+              //   <div className="invalid-feedback text-start">
+              //     {errors.customerName?.types.pattern}
+              //   </div>
+              // </>
+              Object.entries(errors.customerName.types).map(([type, msg]) => (
+                <div className="invalid-feedback text-start" key={type}>
+                  {msg}
+                </div>
+              ))}
           </div>
         </div>
 
@@ -87,13 +129,40 @@ function FoodDeliveryForm() {
           <div className="form-floating">
             <input
               type="email"
-              className="form-control"
+              className={`form-control ${errors.email ? "is-invalid" : ""}`}
               placeholder="Email ..."
               {...register("email", {
                 required: "Email is required.",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Incorrect email format.",
+                },
+                validate: {
+                  notFake: (value) => {
+                    return (
+                      value !== "email@gmail.com" || "Don't use fake email."
+                    );
+                    // if (value === "email@gmail.com")
+                    //   return "Particular email is blocked.";
+                    // else return true;
+                  },
+                  notFromBlackListedDomain: (value, values) => {
+                    // values.customerName
+                    return (
+                      (!value.endsWith("@xyz.com") &&
+                        !value.endsWith("@example.com")) ||
+                      "Domain is blocked."
+                    );
+                  },
+                },
               })}
             />
             <label>Email</label>
+            {errors.email && (
+              <div className="invalid-feedback text-start">
+                {errors.email?.message}
+              </div>
+            )}
           </div>
         </div>
       </div>
